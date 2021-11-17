702c820a088135d247a573b0a29489ebb96c81c0
Bienvenu Muheshimiwa <bienvenumuheshimiwa30@gmail.com>
2021-11-17 13:25:51 +0200

    js commit

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ BienvenuMuheshimiwa.school= "Yale University"
 BienvenuMuheshimiwa.favouriteArtist="Joé Dwèt filé"
 BienvenuMuheshimiwa ['ability'] ="all around "
 console.log (BienvenuMuheshimiwa);
+
 // let x=10
 // let y=15
 // let z= x+y 
@@ -152,193 +153,372 @@ console.log (BienvenuMuheshimiwa);
 //   }
 //   console.log(Joé)
   
-  let arrayOf = []
-  for (let of =4; of>0; of-=1 ) {
-      arrayOf.push(of)
-  }
-  console.log (arrayOf)
+//   let arrayOf = []
+//   for (let of =4; of>0; of-=1 ) {
+//       arrayOf.push(of)
+//   }
+//   console.log (arrayOf)
 
-let MyName = "Bienvenu is "
-let age = "22"
-let result = MyName + age 
-console.log(result)
-   let myArray = [ "name", "lastname, nickname"]
-  myArray[0]= "coolname";
-  myArray.push= ("theonly");
-  myArray.shift=("bitch i am boss")
-  console.log(myArray)
-   let myShop= [["burger", "pizza"],["tomato", "fruits"]]
-   console.log(myShop)
+// let MyName = "Bienvenu is "
+// let age = 22 
+// let result = MyName + age 
+// console.log(result)
+//    let myArray = [ "name", "lastname, nickname"]
+//   myArray[0]= "coolname";
+//   myArray.push= ("theonly");
+//   myArray.shift=("bitch i am boss")
+//   console.log(myArray)
+//    let myShop= [["burger", "pizza"],["tomato", "fruits"]]
+//    console.log(myShop)
 
-   function firstFonction (a,b) {
-       return a+b+9
-   }
+//    function firstFonction (a,b) {
+//        return a+b+9
+//    }
    
-   console.log(firstFonction(2,9))
+//    console.log(firstFonction(2,9))
 
-   function ifStat (stat) {
-       if (stat) {
-           return "i got it"
-       }
-       return "no bro"
+//    function ifStat (stat) {
+//        if (stat) {
+//            return "i got it"
+//        }
+//        return "no bro"
 
-   }
-   console.log(ifStat(true))
+//    }
+//    console.log(ifStat(true))
    
-   function comparison (v) {
-       if (v>1) {
-       return "pass the weed"
-    }
-    return "keep the weed"
-   }
-   console.log(comparison(0.4))
+//    function comparison (v) {
+//        if (v>1) {
+//        return "pass the weed"
+//     }
+//     return "keep the weed"
+//    }
+//    console.log(comparison(0.4))
    
-   function doubleComp (k) {
-       if (k<=3 ||  k>4) {
-           return "i got it all"
-       }
-       return "still i got it all"
-   }
-   console.log(doubleComp(9))
+//    function doubleComp (k) {
+//        if (k<=3 ||  k>4) {
+//            return "i got it all"
+//        }
+//        return "still i got it all"
+//    }
+//    console.log(doubleComp(9))
     
-   function BienvenuMuhesh (val) {
-       if (val >12) {
-return "superieur"
-       }
-       else if (val == 4 ) {
-           return "largement superieur"
-       }
-       else if (val>6 ) {
-           return " always sup"
-       }
-       else {
-           return "c est bon"
-       }
-   }
-   console.log(BienvenuMuhesh(4))
+//    function BienvenuMuhesh (val) {
+//        if (val >12) {
+// return "superieur"
+//        }
+//        else if (val == 4 ) {
+//            return "largement superieur"
+//        }
+//        else if (val>6 ) {
+//            return " always sup"
+//        }
+//        else {
+//            return "c est bon"
+//        }
+//    }
+//    console.log(BienvenuMuhesh(4))
 
 
-   function switchStatement (val) {
-       switch (val) {
-    case 1:
-    case 2:    
-           return "you got it"
+//    function switchStatement (val) {
+//        switch (val) {
+//     case 1:
+//     case 2:    
+//            return "you got it"
    
-       case 4:
-        case 6:
-           return "still got it"
-       case 8:
-        case 9:
-           return "yeah"  
-        default :
-        return "bro"
-        }
-   }
-   console.log(switchStatement(6))
+//        case 4:
+//         case 6:
+//            return "still got it"
+//        case 8:
+//         case 9:
+//            return "yeah"  
+//         default :
+//         return "bro"
+//         }
+//    }
+//    console.log(switchStatement(6))
     
-   function exercice (valeur) {
-       switch (valeur) {
-           case "bob" :
-        return "Marley";
-        case 42 :
-            return "the answer";
-            case  46 :
-            return "there is no 1 "
-default :
-return "whatever"
-       }
+//    function exercice (valeur) {
+//        switch (valeur) {
+//            case "bob" :
+//         return "Marley";
+//         case 42 :
+//             return "the answer";
+//             case  46 :
+//             return "there is no 1 "
+// default :
+// return "whatever"
+//        }
         
-   }
-   console.log (exercice("b"))
+//    }
+//    console.log (exercice("b"))
 
-   function isless (a,b) {
-       return a<b
-   }
-   console.log(isless(10,12))
+//    function isless (a,b) {
+//        return a<b
+//    }
+//    console.log(isless(10,12))
   
-   const myThings = { "phone": " Samsung galaxy", "girl" : 2 }
-   myThings.phone= "Iphone";
-   myThings.girl= 0
-   myThings['ability']= "all around"
-   var phoneUsed = myThings['phone']
-   console.log(phoneUsed)
-   let nbaPlayers = { 23: "LBJ", 0: "Russel Westbrook"}
-   let numbers = 23
-let lebron = nbaPlayers[numbers]
-console.log(lebron)
+//    const myThings = { "phone": " Samsung galaxy", "girl" : 2 }
+//    myThings.phone= "Iphone";
+//    myThings.girl= 0
+//    myThings['ability']= "all around"
+//    var phoneUsed = myThings['phone']
+//    console.log(phoneUsed)
+//    let nbaPlayers = { 23: "LBJ", 0: "Russel Westbrook"}
+//    let numbers = 23
+// let lebron = nbaPlayers[numbers]
+// console.log(lebron)
    
-   function someNames (val) {
-    //    var result = ""
-       var nom = {
-           "Bienvenu": "Muheshimiwa",
-           "lebron" : "james",
-       }
-       result = nom[val];
-       return result;
-   }
+//    function someNames (val) {
+//     //    var result = ""
+//        var nom = {
+//            "Bienvenu": "Muheshimiwa",
+//            "lebron" : "james",
+//        }
+//        result = nom[val];
+//        return result;
+//    }
   
-   var myplants = [
-       {
-           type: "flower",
-           list: [
-               "rose",
-               "dandelion",
-               "tulip"
-           ]
-       },
-       {
-            type:"trees",
-            list: [
-                "fir",
-                "pine",
-                "birch",
-            ]
+//    var myplants = [
+//        {
+//            type: "flower",
+//            list: [
+//                "rose",
+//                "dandelion",
+//                "tulip"
+//            ]
+//        },
+//        {
+//             type:"trees",
+//             list: [
+//                 "fir",
+//                 "pine",
+//                 "birch",
+//             ]
 
-       }
-   ]
-   var secondTree = myplants[1].list[2]
-   console.log(secondTree)
+//        }
+//    ]
+//    var secondTree = myplants[1].list[2]
+//    console.log(secondTree)
 
-   let myLoop = [];
-   var i = 13;
-   while (i < 100) {
-   myLoop.push(i);
-   i+=5}
-   console.log(myLoop)
+//    let myLoop = [];
+//    var i = 13;
+//    while (i < 100) {
+//    myLoop.push(i);
+//    i+=5}
+//    console.log(myLoop)
 
-let ourLoop = []
-for (var i = 1; i<10; i+=2) {
-    ourLoop.push(i)
-}
-console.log(ourLoop)
+// let ourLoop = []
+// for (var i = 1; i<10; i+=2) {
+//     ourLoop.push(i)
+// }
+// console.log(ourLoop)
 
 
-//  function myTarget (val) {
-//   var val = prompt ("Tell us about your age")
-//     if (val >= 18 && val <= 33 ){
-//     return "my target" };
-//     return  "not my audience";
+// //  function myTarget (val) {
+// //   var val = prompt ("Tell us about your age")
+// //     if (val >= 18 && val <= 33 ){
+// //     return "my target" };
+// //     return  "not my audience";
     
-// }
-// console.log(myTarget())
-//  val = prompt ("ff");
-//  myTarget();
-//  console.log(val)
-// function greeting () {
-//  var nom  = prompt ( 'Your name');
-//   var result = "Bonjour" + " " + nom;
+// // }
+// // console.log(myTarget())
+// //  val = prompt ("ff");
+// //  myTarget();
+// //  console.log(val)
+// // function greeting () {
+// //  var nom  = prompt ( 'Your name');
+// //   var result = "Bonjour" + " " + nom;
  
-//   console.log(result) ;
-// }
-//  greeting()
-;
-function easy (va) {
-    //  var va = prompt ('Tell us your price')
-     if (va >= 5000) {
-         return "Pleased with the deal "
-     };
-     return "go away";
+// //   console.log(result) ;
+// // }
+// //  greeting()
+// ;
+// function easy (va) {
+//     //  var va = prompt ('Tell us your price')
+//      if (va >= 5000) {
+//          return "Pleased with the deal "
+//      };
+//      return "go away";
      
-}
-console.log(easy())
-console.log(document.querySelectorAll('.button')) 
+// }
+// console.log(easy())
+// console.log(document.querySelectorAll('.button')) 
+
+//  let  school = (a) => {
+//     // a = prompt ("what is your amount")
+//     if ( a >20) {
+//      return "this is an arrow function"}
+//      return "no"
+//  }
+//  console.log(school())
+//  var name = " Bienvenu Muheshimiwa"
+//  var age = 22 
+
+
+//   
+//  let myLoop = 10;
+//   while ( myLoop > 0) {
+//       console.log ( 'i have' + myLoop + " gf" )
+//       myLoop--
+//   }
+    
+   
+// let i ;
+//    for ( i = 20; i > 0; i--) {
+//        console.log( 'i have' + i+ 'gf');
+//    }
+   
+//     let myAttributes = { lawyering : "perfect", 
+//     coding: " almost there " , 
+//     myIq,
+    
+//  }
+//   console.log(myAttributes.myIq (200))
+//   var allAtt= `myAttributes.myIq(200)  ${myAttributes.coding.toUpperCase().length} ${ 'and it feels good'}`
+//   console.log(allAtt)
+ 
+//  function  myIq (val) {
+//     if (val >190) 
+//     return "genius"; 
+//     else {
+//         return  "dumb"}}
+//         // myIq (200)
+
+// const orga = []
+//     let myArray = [ "faculté de droit", " faculté de agronomie", " faculté d'économie"]
+//      myArray.push = " faculté d'archicteture "
+//      console.log(myArray.length)
+//      console.log(myArray)
+//      let dean = [ " Bahala :", "Landry:", "Muhunduka:", "inconnue:"]
+//      const ucb = myArray.concat(dean)
+//  for ( let i=0; i<myArray.length; i++) {
+//      const doyens = ` ${myArray[i]} ${dean}`
+// // orga.push(doyens)
+//  }
+//  for (let i=0; i<dean.length; i++) {
+//      const prof = `${dean[i]} ${myArray[i]}`
+//      orga.push (prof)
+//  }
+//      console.log(orga)
+
+//      const names = [ " anna", "suzim", "bob"];
+//      const lastNames = "carter"
+//      const emptyArray = []
+//         for (let i=0; i<names.length; i++) {
+//         const both = `${names[i]} ${lastNames} `
+// emptyArray.push (both)
+//         }
+//      console.log(emptyArray)
+      
+  
+// const mesCourses1 = [ 400,  600, 900]
+// const frais2 = [ 100, 200, 400]
+//  function calcul (arr) {
+//   let total = 0;  
+//     for ( let i=0; i<arr.length; i++) {
+//         total += arr[i]
+//     }
+//     // return total;
+ 
+//   if (total > 500) {
+//       console.log ( " punguza depense");
+// return total;}
+//    console.log ("keep going")
+      
+//   }
+//  var total1= calcul(frais2)
+//  console.log(total1) 
+
+
+
+//   function callBack (name) {
+//     // console.log ("Good morning Bob")
+//     return  `Good morning  ${name}`
+//   }
+   
+//   function greet ( name,b ) {
+//       var lastNames = "Muheshimiwa";
+//     console.log (`${b (name)} my name is ${lastNames}`);
+    
+//     //   return 
+//   }
+//    console.log(greet("john", callBack));
+
+
+
+//    function girlFriend (x) {
+//    let bb=  `Elle est trop belle ${x}`
+//     return bb 
+//    }
+//    function  moi (x,y) {
+//        let my = "Bienvenu"
+//        console.log ( `${y(x)} mon grand ${my}` )
+//    }
+//    console.log (moi ("julienne", girlFriend))
+//    console.log (moi ("julia", girlFriend))
+
+//  const people = [
+//      {theName: "Bob", age: 25, position: 'backend ', salary : 9000 },
+//      {theName: "peter", age: 20, position: 'front end' ,salary : 8000},
+//      {theName: "parker", age: 27, position: 'full stack ',salary : 6000},
+//      {theName: "Dean", age: 30, position: 'designer',salary : 7000},
+//  ];
+
+//   people.forEach(  (person) =>  { console.log(person.theName) });
+
+//   const co = people.map(function (person){
+// return  {
+//    oldage: person.age+23,
+//     myName : person.theName,
+// }
+// });
+//   console.log(co)
+//     const newArray = people.filter( function (person) {
+//         return person.age > 25 
+//     })
+//    console.log(newArray) 
+
+    
+   const companies = [
+       { nom:"company one", category: "finance", start: 1981, end : 2003 },
+       { nom:"company two", category: "education", start: 1992, end : 2005 },
+       { nom:"company three", category: "health", start: 1953, end : 2006 },
+       { nom:"company for", category: "agriculture", start: 1884, end : 2007 },
+       { nom:"company five", category: "technology", start: 1975, end : 2008 },
+       { nom:"company six", category: "building", start: 1926, end : 2009 },
+       { nom:"company seven", category: "music", start: 1947, end : 2010 },
+       { nom:"company eight", category: "movie", start: 1908, end : 2011 },
+       { nom:"company nine", category: "theater", start: 1940, end : 2012 },
+   ]
+    // companies.forEach(function (tech) {
+    //     console.log(tech);
+     
+  let emptyArray=  companies.map((someNames) => {
+      return {
+        "IBM": someNames.nom };
+    })
+
+console.log(emptyArray)
+    // const ages = [ 12,13,14,15,16,17,18,19,20]
+     
+    // const newAges= ages.filter(function(myAges) {
+    //      return myAges>13
+    //  })
+    //  console.log(newAges)
+    //  const newComp = companies.filter (function(duration) {
+    //      if (duration.end - duration.start > 50)
+    //      return true
+           
+         
+    //  })
+    //  console.log(newComp);
+    
+    //  const emptyArrays= []
+    //  for (let i=0;i<ages.length; i++) {
+    //     if (ages[i] >12) {
+    //         emptyArrays.push(ages[i])
+    //     } ;
+        
+        
+    //  }
+    //  console.log(emptyArrays )
